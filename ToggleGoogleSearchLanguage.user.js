12eb7d36d8dd42d9aa2adf0003b47d4bad60adf4
tanabe <tanablog@gmail.com>
2011-05-25 00:16:22 +0900

    update

--- ToggleGoogleSearchLanguage.user.js
+++ ToggleGoogleSearchLanguage.user.js
@@ -7,18 +7,31 @@
 // @include        http://www.google.com/*
 // ==/UserScript==
 (function() {
-  document.addEventListener('dblclick', function(event) {
+  document.addEventListener('dblclick', function(e) {
     var next;
+    //new interface
     if (location.href.match(/#hl=(ja|en)/)) {
       next = RegExp.$1 === "ja" ? "en" : "ja";
+    //old interface
     } else if (location.pathname.match(/^\/search/)) {
-      next = location.search.match(/hl=ja/) ? "en" : "ja";
+      if (location.href.match(/hl=ja/)) {
+        next = "en";
+      } else if (location.href.match(/hl=en/)) {
+        next = "ja";
+      } else if (location.href.match(/google\.co\.jp\//)) {
+        next = "en";
+      } else {
+        next = "ja";
+      }
     }
 
+    var nextURL;
     if (next === "ja") {
-      location.href = location.href.replace(/google\.com\//, "google.co.jp/").replace(/hl=en/g, "hl=ja");
+      nextURL = location.href.replace(/google\.com\//, "google.co.jp/").replace(/hl=en/g, "hl=ja");
     } else {
-      location.href = location.href.replace(/google\.co\.jp\//, "google.com/").replace(/hl=ja/g, "hl=en");
+      nextURL = location.href.replace(/google\.co\.jp\//, "google.com/").replace(/hl=ja/g, "hl=en");
     }
+    location.href = nextURL;
   }, false);
 })();
+
